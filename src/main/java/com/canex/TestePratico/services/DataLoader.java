494b8dbea0b4284be42501575ad12b3a205155c3
JoaoVitorAR <jvalves1998@outlook.com.br>
2024-01-09 17:41:47 -0300

    Finalização das validações dos campos do formulario de cadastro de venda

--- src/main/java/com/canex/TestePratico/services/DataLoader.java
+++ src/main/java/com/canex/TestePratico/services/DataLoader.java
@@ -25,36 +25,96 @@ public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
     public void onApplicationEvent(ContextRefreshedEvent event) {
        if(produtos_repository.count() == 0){
         var a = new Produto();
-        a.setDescricao("Prod A");
-        a.setPreco(new BigDecimal(3.20));
+        a.setDescricao("Arroz branco");
+        a.setPreco(new BigDecimal(15.00));
         produtos_repository.save(a);
 
         var b = new Produto();
-        b.setDescricao("Prod B");
-        b.setPreco(new BigDecimal(1.30));
+        b.setDescricao("Macarrão espaguete");
+        b.setPreco(new BigDecimal( 4.00));
         produtos_repository.save(b);
 
         var c = new Produto();
-        c.setDescricao("Prod C");
-        c.setPreco(new BigDecimal(5.80));
+        c.setDescricao("Leite integral");
+        c.setPreco(new BigDecimal(3.50));
         produtos_repository.save(c);
+
+        var d = new Produto();
+        d.setDescricao("Ovos");
+        d.setPreco(new BigDecimal(6.00));
+        produtos_repository.save(d);
+
+        var e = new Produto();
+        e.setDescricao("Peito de frango");
+        e.setPreco(new BigDecimal(13.00));
+        produtos_repository.save(e);
+
+        var f = new Produto();
+        f.setDescricao("Maçãs");
+        f.setPreco(new BigDecimal(4.50));
+        produtos_repository.save(f);
+
+         var g = new Produto();
+        g.setDescricao("Sabonete líquido");
+        g.setPreco(new BigDecimal(8.00));
+        produtos_repository.save(g);
+
+        var h = new Produto();
+        h.setDescricao("Detergente");
+        h.setPreco(new BigDecimal(2.50));
+        produtos_repository.save(h);
+
+        var i = new Produto();
+        i.setDescricao("Shampoo");
+        i.setPreco(new BigDecimal(12.50));
+        produtos_repository.save(i);
+
+        var j = new Produto();
+        j.setDescricao("Papel higiênico");
+        j.setPreco(new BigDecimal(6.00));
+        produtos_repository.save(j);
        }
 
-       if(clientes_repository.count() ==0){
+       if(clientes_repository.count() == 0){
         var a = new Cliente();
-        a.setNome("Cliente A");
+        a.setNome("Ana Silva");
         a.setCnpj("42.537.400/0001-80");
         clientes_repository.save(a);
 
         var b = new Cliente();
-        b.setNome("Cliente A");
+        b.setNome("Carlos Mendes");
         b.setCnpj("28.394.131/0001-10");
         clientes_repository.save(b);
 
         var c = new Cliente();
-        c.setNome("Cliente C");
+        c.setNome("Sofia Oliveira");
         c.setCnpj("06.095.609/0001-78");
         clientes_repository.save(c);
+
+        var d = new Cliente();
+        d.setNome("João Santos");
+        d.setCnpj("13.703.240/0001-02");
+        clientes_repository.save(d);
+
+        var e = new Cliente();
+        e.setNome("Marina Fernandes");
+        e.setCnpj("87.283.842/0001-86");
+        clientes_repository.save(e);
+
+        var f = new Cliente();
+        f.setNome("Pedro Rodrigues");
+        f.setCnpj("68.742.019/0001-47");
+        clientes_repository.save(f);
+
+        var g = new Cliente();
+        g.setNome("Laura Costas");
+        g.setCnpj("21.868.949/0001-88");
+        clientes_repository.save(g);
+
+        var h = new Cliente();
+        h.setNome("Miguel Almeida");
+        h.setCnpj("84.602.546/0001-02");
+        clientes_repository.save(h);
        }
     }
     
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,15 +2,13 @@ const dataAtual = new Date();
 const dataFormatada = `${String(dataAtual.getDate()).padStart(2, '0')}/${String(dataAtual.getMonth() + 1).padStart(2, '0')}/${dataAtual.getFullYear()}`;
 document.getElementById('dataFormatada').textContent = `${dataFormatada}`;
 
-function buscarCEP() {
-    
+function buscarCEP() {  
     let cep = document.getElementById('cep').value;
     let url = `https://viacep.com.br/ws/${cep}/json/`;
     fetch(url)
         .then(response => response.json())
         .then(data => exibirResultado(data))
-        .catch(error => console.error('Erro ao buscar CEP:', error));
-
+        .catch(error => alert("CEP não encontrado ou informado de forma inválida."));
 }
 
 function exibirResultado(data) {
@@ -30,23 +28,30 @@ function InserirProduto() {
     let total = obterValorElemento();
     var preco_unitario = 0;
     var valorTotalProduto = 0;
+
+    if (quantidade == ''){
+        alert("Insira a quantidade de itens do produtos");
+    } else {
     
-    $.ajax({
-        url: 'RecuperaValorProdutoPorId?produtoId=' + produtoId,
-        type: 'get',
-        DataType: 'json',
-        success: function(preco) {
-            preco_unitario = preco;
-            valorTotalProduto = quantidade * preco;        
-            let valorTotal = parseFloat(total);
-            valorTotal += (quantidade * preco);
-            document.getElementById('listaProdutos').value = valorTotal.toFixed(2);    
-            preencheTabela(produtoId, produto, preco_unitario, quantidade, valorTotalProduto);
-        },
-        error: function(e) {
-            console.error(e);
-        }
-    });
+        $.ajax({
+            url: 'RecuperaValorProdutoPorId?produtoId=' + produtoId,
+            type: 'get',
+            DataType: 'json',
+            success: function(preco) {
+                preco_unitario = preco;
+                valorTotalProduto = quantidade * preco;        
+                let valorTotal = parseFloat(total);
+                valorTotal += (quantidade * preco);
+                document.getElementById('totalCompra').value = valorTotal.toFixed(2);
+
+                preencheTabela(produtoId, produto, preco_unitario, quantidade, valorTotalProduto);
+                document.getElementById('quantidade').value = '';
+            },
+            error: function(e) {
+                alert("Erro ao preencher a tabela: ", e);
+            }
+        });
+    }
        
 }
 
@@ -74,48 +79,48 @@ function preencheTabela(codigo_produto, descricao_produto, preco_unitario, quant
     cel1.innerHTML = String(num_item).padStart(3,0);
     cel2.innerHTML = codigo_produto;
     cel3.innerHTML = descricao_produto;
-    cel4.innerHTML = preco_unitario;
+    cel4.innerHTML = preco_unitario.toFixed(2);;
     cel5.innerHTML = quantidade;
-    cel6.innerHTML = total_produto;  
+    cel6.innerHTML = total_produto.toFixed(2);;  
 }
 
 function obterValorElemento() {
-    return parseFloat(document.getElementById('listaProdutos').value) || 0;
+    return parseFloat(document.getElementById('totalCompra').value) || 0;
 }
 
-
-$(document).ready(function(){
+$(document).ready(function(){  
     $("#formVenda").submit(function(event){
-        event.preventDefault();
+        if ($("#totalCompra").val() != 0){
+            event.preventDefault();
 
-        var clienteId = document.getElementById("cliente");
-
-        var formData = {
-            id: 0,
-            num_item: 0,
-            produto_id: 0,
-            cliente_id: clienteId.value,
-            quantidade: 0,
-            total_compra: $("#listaProdutos").val(),
-            cep: $("#cep").val(),
-            endereco: $("#resultadoCEP").val()
-        }
-        console.log(formData)
-
-        $.ajax({
-            type: "POST",
-            url: "/InsertVendas",
-            data: formData,
-            success: function(data) {
-                alert("Venda realizada com sucesso.");
-            },
-            error: function(xhr, status, error) {
-                var responseJSON = JSON.parse(xhr.responseText);
-                var errorMessage = responseJSON.errorMessage;
-                alert("Erro: " + errorMessage);
+            var clienteId = document.getElementById("cliente");
+            var formData = {
+                id: 0,
+                cliente_id: clienteId.value,
+                total_compra: $("#totalCompra").val(),
+                cep: $("#cep").val(),
+                endereco: $("#resultadoCEP").val()
             }
-        });
 
-        window.location.href="/listarVendas"
+            $.ajax({
+                type: "POST",
+                url: "/InsertVendas",
+                data: formData,
+                success: function(data) {
+                    alert("Venda realizada com sucesso.");
+                },
+                error: function(xhr, status, error) {
+                    var responseJSON = JSON.parse(xhr.responseText);
+                    var errorMessage = responseJSON.errorMessage;
+                    alert("Erro: " + errorMessage);
+                }
+            });
+
+            window.location.reload();
+            window.location.href="/listarVendas"
+            
+        } else {
+            alert("Insira ao menos um item para venda");
+        }
     });
 });
